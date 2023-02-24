c45e9b55f713c17c3023c976c4719963cc9943d0
Daine Mawer <hello@dainemawer.com>
2023-02-24 13:39:00 +0200

    feat: add custom document and error templates

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+import { Button } from '@components'
+
+const Custom404: FC = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <Button>Children</Button>
+        </div>
+    )
+}
+
+export default Custom404
