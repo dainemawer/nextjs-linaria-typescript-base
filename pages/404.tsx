6c16ca6e5c9135c60a2e83b02978c359a316a023
Daine Mawer <hello@dainemawer.com>
2023-02-24 13:40:19 +0200

    fix: remove button from file

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,9 @@
 import { FC } from 'react'
-import { Button } from '@components'
 
 const Custom404: FC = () => {
     return (
         <div>
             <h1>404</h1>
-            <Button>Children</Button>
         </div>
     )
 }
